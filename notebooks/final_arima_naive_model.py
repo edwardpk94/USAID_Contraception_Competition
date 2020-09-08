dff7b2cbbd0ca3727119681efa540a346f584a42
Edward Kunkel <edward.pk94@gmail.com>
2020-09-07 22:27:42 -0400

    Adding predictions and final jupyter notebook

--- notebooks/final_arima_naive_model.py
+++ notebooks/final_arima_naive_model.py
@@ -5,15 +5,9 @@ Created on Mon Aug 31 22:56:27 2020
 @authors: anbarry, Eddie Kunkel
 """
 
-
-import warnings 
-warnings.filterwarnings('ignore')
+import time
 import numpy as np 
 import pandas as pd 
-#%matplotlib inline
-import matplotlib.pyplot as plt  
-import seaborn as sns
-import statsmodels.api as sm
 from statsmodels.tsa.arima.model import ARIMA
 from datetime import datetime
 from itertools import product
@@ -40,9 +34,6 @@ def MASE(training_series, testing_series, prediction_series):
     errors = np.abs(testing_series - prediction_series )
     return errors.mean()/d
 
-color = sns.color_palette()
-sns.set_style('darkgrid')
-
 ### import data and adjust to only necessary cols
 full_df = pd.read_csv(r'../data/ProcessingOutput/contraceptive_logistics_data_clean.csv')
 full_df['date'] = pd.to_datetime(full_df['date'], format="%Y-%m-%d")
@@ -61,32 +52,24 @@ full_df = full_df.set_index('date', drop = False)
 full_df.index.names = ['date_index']
 full_df = full_df[['site_code','product_code','stock_distributed']]
 
-### Creates the iteration of hyperparamater list
-# Ande's original parameter space
-# AR = [0,6,12]
-# S=[0]
-# MA= [0,6,12]
+AR = [0,1,3,6]
+I=[0,1]
+MA= [0,1,3,6]
 
-AR = [0,1,4]
-S=[0,1]
-MA= [0,1,4]
+# !#EPK DEBUG PARAMS
+AR = [3,6]
+I=[0,1]
+MA= [3,6]
 
-a = [AR,S,MA]
+a = [AR,I,MA]
 arima_params_combinations = list(product(*a))
-
-import time
-
-t0 = time.time()
-t1 = time.time()
-
-total = t1-t0
 
 unique_site_codes = full_df['site_code'].unique()
 unique_product_codes = full_df['product_code'].unique()
 
 # !#EPK DEBUG Code to randomly sample some of the site codes for testing
-unique_site_codes = np.random.choice(unique_site_codes, 1, replace=False)
-unique_product_codes = np.random.choice(unique_product_codes, 2, replace=False)
+unique_site_codes = np.random.choice(unique_site_codes, 6, replace=False)
+unique_product_codes = np.random.choice(unique_product_codes, 1, replace=False)
 
 # Record MASE and model parameters. Will be used for model selection
 eval_df_list = []
@@ -119,11 +102,11 @@ for site_code_dx, site_code in enumerate(unique_site_codes):
         test_df_p = current_df[current_df.index > '2019-06-01']
 
         # Test df must have no NaNs so error metrics can be calculated
-        test_df_p.loc[:,'stock_distributed'] = test_df_p.loc[:,'stock_distributed'].fillna(0)
+        # test_df_p.loc[:,'stock_distributed'] = test_df_p.loc[:,'stock_distributed'].fillna(0)
 
         # Calculate the naive solution by averaging the 3 most recent months
         test_df_p['naive_pred'] = train_df_p[~train_df_p['stock_distributed'].isna()]['stock_distributed'].tail(3).mean()
-        naive_MASE = MASE(train_df_p['stock_distributed'].fillna(0), test_df_p['stock_distributed'], test_df_p['naive_pred'])
+        naive_MASE = MASE(train_df_p['stock_distributed'].fillna(0), test_df_p['stock_distributed'].fillna(0), test_df_p['naive_pred'].fillna(0))
 
         best_ARIMA_MASE = None
         best_ARIMA_pred = None
@@ -141,7 +124,7 @@ for site_code_dx, site_code in enumerate(unique_site_codes):
                     arima_model = ARIMA(train_df_p.stock_distributed, order=arima_params).fit()
 
                     arima_pred = arima_model.predict(start=datetime(2019,7,1), end=datetime(2019,9,1))
-                    model_MASE = MASE(train_df_p['stock_distributed'].fillna(0), test_df_p['stock_distributed'], arima_pred)
+                    model_MASE = MASE(train_df_p['stock_distributed'].fillna(0), test_df_p['stock_distributed'].fillna(0), arima_pred)
 
                     # Store the parameters of the best-performing ARIMA model
                     if (not best_ARIMA_MASE) or (model_MASE < best_ARIMA_MASE):
@@ -174,71 +157,63 @@ for site_code_dx, site_code in enumerate(unique_site_codes):
 
 # DF with data and predictions for all site/product codes run
 combo_df = pd.concat(combo_df_list)
-combo_df.drop(columns=['stock_distributed'], inplace=True)
+
+ARIMA_MASE = MASE(combo_df['stock_distributed'].fillna(0), combo_df['stock_distributed'], combo_df['ARIMA_pred'])
+print('Overall MASE for ARIMA predictions: {}'.format(ARIMA_MASE))
+
+Naive_MASE = MASE(combo_df['stock_distributed'].fillna(0), combo_df['stock_distributed'], combo_df['naive_pred'])
+print('Overall MASE for Naive predictions: {}'.format(Naive_MASE))
 
 eval_df = pd.concat(eval_df_list)
 eval_df['ARIMA_performed_better'] = eval_df['ARIMA_MASE'] < eval_df['naive_MASE']
 
-
-# !#EPK Can remove this when testing all product/site codes. Should be the same number of entries as the original full_df
-full_df = pd.merge(full_df, combo_df, how='outer', on=['date_index','site_code','product_code'])
-
-print(eval_df)
+full_df = pd.merge(full_df, combo_df.drop(columns=['stock_distributed']), how='outer', on=['date_index','site_code','product_code'])
+full_df.to_csv('../predictions/full_df.csv')
 
 # Draft final predictions using the best performing model
 predictions_df_list = []
 prediction_dates = pd.date_range(start=datetime(2019,10,1), end=datetime(2019,12,1), freq='MS')
-
+USE_NAIVE_ONLY = False
 for site_code_dx, site_code in enumerate(unique_site_codes):
     for product_code_dx, product_code in enumerate(unique_product_codes):
         curr_df = full_df.loc[(full_df['site_code'] == site_code) & (full_df['product_code'] == product_code),:]
+        curr_df.sort_index(inplace=True)
         curr_eval = eval_df.loc[(eval_df['site_code'] == site_code) & (eval_df['product_code'] == product_code),:]
         curr_prediction = pd.DataFrame(index=prediction_dates)
         curr_prediction['site_code'] = site_code
         curr_prediction['product_code'] = product_code
 
         # Naive model prediction
-        curr_naive_pred = curr_df[~curr_df['stock_distributed'].isna()]['stock_distributed'].mean()
+        curr_naive_pred = curr_df[~curr_df['stock_distributed'].isna()]['stock_distributed'].tail(3).mean()
         
         if np.isnan(curr_naive_pred):
             curr_naive_pred = 0
 
+        if USE_NAIVE_ONLY:
+            curr_prediction['predicted_value'] = curr_naive_pred
+            predictions_df_list.append(curr_prediction)
+            continue
+
+        set_trace()
+        
         # ARIMA model prediction
         if (curr_eval['ARIMA_performed_better'].iloc[0]) & (curr_eval['num_non_nan_train_dates'].iloc[0] > 0):
             try:
-                arima_model = ARIMA(curr_df['stock_distributed'], order=curr_eval['ARIMA_params']).fit()
+                arima_model = ARIMA(curr_df['stock_distributed'], order=curr_eval['ARIMA_params'].iloc[0]).fit()
                 arima_pred = arima_model.predict(start=datetime(2019,10,1), end=datetime(2019,12,1))
-                curr_prediction['prediction'] = arima_pred
+                curr_prediction['predicted_value'] = arima_pred
             except:
+                print('ARIMA model failed... Substituting with naive prediction')
                 # Use naive model if ARIMA fails to train
-                curr_prediction['prediction'] = curr_naive_pred
+                curr_prediction['predicted_value'] = curr_naive_pred
         else:
-            curr_prediction['prediction'] = curr_naive_pred
+            curr_prediction['predicted_value'] = curr_naive_pred
 
         predictions_df_list.append(curr_prediction)
 
 predictions = pd.concat(predictions_df_list)
+predictions['year'] = predictions.index.year
+predictions['month'] = predictions.index.month
+predictions = predictions[['year','month','site_code','product_code','predicted_value']]
+predictions.to_csv('../predictions/predictions.csv', index=False)
 set_trace()
-
-
-### shows MAPE SMAP and MASE for entire test file
-def smape_kun(training, y_true, y_pred):
-    mase = MASE(training, y_true, y_pred)
-    mape = np.mean(abs((y_true-y_pred)/y_true))*100
-    smape = np.mean((np.abs(y_pred - y_true) * 200/ (np.abs(y_pred) + np.abs(y_true))).fillna(0))
-    print('MAPE: %.2f %% \nSMAPE: %.2f %% \nMASE: %.2f'% (mape,smape,mase), "%")
-
-### Creates a list of individual MASEs per SITE/Product
-# mase_list = []
-# for site_code in list(test['site_code'].unique()):
-#     print("on store "+str(y)+" out of "+str(len(list(test['site_code'].unique()))))
-#     test_df = test[test['site_code']==site_code]
-#     train_df = train[train['site_code']==site_code]
-#     for product_code in list(test_df['product_code'].unique()):
-#         train_df_p = train_df[train_df['product_code']==product_code]
-#         test_df_p = test_df[test_df['product_code']==product_code]
-#         mase = MASE(train_df_p['stock_distributed'], test_df_p['stock_distributed'], test_df_p['ARIMA_Pred'])
-#         mase_list.append(mase)
-
-### Shows just the individual MASE
-print(MASE(train['stock_distributed'], test['stock_distributed'], test['ARIMA_Pred']))
